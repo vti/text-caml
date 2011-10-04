1b0abb65c214320c49150df8bc05ec314e841ebf
vti <viacheslav.t@gmail.com>
2011-10-04 22:05:08 +0300

    Fixed version

--- lib/Text/Caml.pm
+++ lib/Text/Caml.pm
@@ -7,7 +7,7 @@ require Carp;
 require Scalar::Util;
 use File::Spec ();
 
-our $VERSION = '0.009004';
+our $VERSION = '0.00905';
 
 our $LEADING_SPACE  = qr/(?:\n [ ]*)?/x;
 our $TRAILING_SPACE = qr/(?:[ ]* \n)?/x;
