9b76d6c381dfe08c47e24494b43ad37908528684
vti <viacheslav.t@gmail.com>
2018-08-31 22:16:31 +0200

    Checking in changes prior to tagging of version 0.17.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 65f72a0..7b4f9c0 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for perl module Text::Caml
    
     {{$NEXT}}
    
    +0.17 2018-08-31T20:16:26Z
    +
    + - Non-escape mode (Igor Lobanov)
    +
     0.16 2017-05-03T10:42:27Z
    
      - Escape "{" and "}" (Shoichi Kaji)

--- lib/Text/Caml.pm
+++ lib/Text/Caml.pm
@@ -7,7 +7,7 @@ require Carp;
 require Scalar::Util;
 use File::Spec ();
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 our $LEADING_SPACE  = qr/(?:\n [ ]*)?/x;
 our $TRAILING_SPACE = qr/(?:[ ]* \n)?/x;
